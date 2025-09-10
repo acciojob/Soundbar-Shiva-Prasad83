292a91b8b85d94fde9bd4f29f3e742d213400db4
Shiva-Prasad83 <shivaprasad.ch83@gmail.com>
2025-09-10 20:58:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 //your JS code here. If required.
+ let sounds=["applause","boo","gasp","tada","victory","wrong","stop"];
+   let audios=document.querySelector(".audios");
+   let container=document.getElementById("buttons");
+   function creatingAudios(){
+    for(let sound of sounds){
+        if(sound!="stop"){
+            let audio=new Audio(`./Audios/${sound}.mp3`);
+            audio.id=sound;
+    // let audio=document.createElement("audio");
+    // audio.src=`./Audios/${sound}.mp3`;
+    // audio.id=sound;
+    audios.appendChild(audio);
+        }
+    }
+   }
+creatingAudios();
+
+   function createButtons(){
+    for(let sound of sounds){
+        let button=document.createElement("button");
+        if(sound!="stop"){     
+        button.className="btn";
+        button.innerText=sound;
+        button.addEventListener("click",()=>{
+           let audio=document.getElementById(sound);
+           stopSounds();
+           audio.play();
+        })
+        }else{
+           
+            button.className="stop";
+            button.innerText=sound;
+            button.addEventListener("click",stopSounds);
+        }
+        container.appendChild(button);
+    }
+   }
+   createButtons();
+   function stopSounds(){
+    for(let sound of sounds){
+        if(sound!="stop"){
+    let audio=document.getElementById(sound);
+    audio.pause();
+    audio.currentTime=0;
+        }
